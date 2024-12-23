013d332310c3ebf10ea95c0ab1b27f0b896abfd4
Jindongleee <jindong1019@gmail.com>
2024-12-23 22:20:24 +0900

    Commit and Push

--- src/A.java
+++ src/A.java
@@ -1,6 +1,6 @@
 public class A {
     public static void main(String[] args) {
         System.out.print("Hello A World!");
-        System.out.println("My name is A.");
+        System.out.println("My name is AC.");
     }
 }
--- src/C.java
+++ src/C.java
@@ -1,5 +1,7 @@
 public class C {
     public static void main(String[] args) {
         System.out.printf("Hello C World!");
+        System.out.print("My name is C.");
+
     }
 }
